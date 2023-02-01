d0936ab5775d634bcd76fd8d3cb74b6c3857d1b6
Dipak Pawar <itsdipakpawar4206@gmail.com>
2023-02-02 00:12:57 +0530

    Mobile Responsiveness fixed

--- Deshmukh-Manufacturing/src/Components/OurServicesCompo/Introservices.jsx
+++ Deshmukh-Manufacturing/src/Components/OurServicesCompo/Introservices.jsx
@@ -7,8 +7,8 @@ const Introservices = () => {
     <div className="vehicalimg pt-20 xs:pt-28     ">
    
     </div>
-       <h1 className='left-72 absolute top-1/3 text-5xl xs:top-1/4 xs:text-xl xs:left-14 text-white font-semibold'>Our Products</h1>
-       <span className="inline-block h-1 w-72  rounded xs:left-14 xs:top-64 bg-red-500 xs:w-28 left-72 absolute top-72 text-5xl text-white font-semibold"   
+       <h1 className='left-72 absolute top-1/3 text-5xl xs:top-1/3 xs:text-2xl xs:left-14 text-white font-semibold'>Our Products</h1>
+       <span className="inline-block h-1 w-72  rounded xs:left-14 xs:top-56 bg-red-500 xs:w-28 left-72 absolute top-72 text-5xl text-white font-semibold"   
           data-aos-anchor-placement="center-bottom"></span>
        </>
   )
